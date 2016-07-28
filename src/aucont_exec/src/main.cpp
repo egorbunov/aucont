316eeb9ac183eb73fe0479487eeb35de26eaa40e
Egor Gorbunov <egor-mailbox@ya.ru>
2016-07-28 15:38:22 +0300

    Fix aucont_exec

--- src/aucont_exec/src/main.cpp
+++ src/aucont_exec/src/main.cpp
@@ -11,6 +11,7 @@
 #include <sched.h>
 #include <array>
 #include <sys/wait.h>
+#include <fstream>
 
 namespace 
 {
@@ -44,17 +45,8 @@ int main(int argc, char* argv[]) {
     }
     auto exe_path = aucont::get_file_real_dir(argv[0]);
     aucont::set_aucont_root(exe_path);
-    auto cgroup_conf_script = exe_path + "apply_cgroup.bash";
 
-    // reading arguments
     std::string cont_pid_str = std::string(argv[1]);
-    char* cmd = argv[2];
-    char* args[100]; // 100 - magic cmd arg maximum
-    for (int j = 2; j < argc; ++j) {
-        args[j - 2] = argv[j];
-    }
-    args[argc - 2] = NULL;
-
     // loading container info
     auto cont = aucont::get_container(stoi(cont_pid_str));
     if (cont.pid == -1) {
@@ -62,62 +54,54 @@ int main(int argc, char* argv[]) {
     }
 
     int synch_pipe[2];
-    int get_slave_pid_pipe[2];
-    if (pipe2(get_slave_pid_pipe, O_CLOEXEC) < 0 ||
-        pipe2(synch_pipe, O_CLOEXEC) < 0) {
+    if (pipe2(synch_pipe, O_CLOEXEC) < 0) {
         aucont::stdlib_error("pipe");
     }
 
-    auto tmp_pid = fork();
-    if (tmp_pid < 0) {
-        aucont::stdlib_error("fork");
-    } else if (tmp_pid > 0) {
-        close(get_slave_pid_pipe[1]);
-        close(synch_pipe[0]);
-        auto slave_proc_pid = aucont::read_from_pipe<pid_t>(get_slave_pid_pipe[0]);
-        std::string pid_str = std::to_string(slave_proc_pid);
-        std::string cgrouph_path = aucont::get_cgrouph_path();
-        if (system(("bash " + cgroup_conf_script + " " + cont_pid_str + " " + pid_str + " " + cgrouph_path).c_str()) != 0) {
-            aucont::error("=(");
-        }
-        aucont::write_to_pipe<bool>(synch_pipe[1], true);
-        if (waitpid(tmp_pid, NULL, 0) < 0) {
-            aucont::stdlib_error("Waitpid failed");
-        }
-        exit(1);
-    }
-
     // applying user and pid namespace and forking first
-    // user first because it sets proper permissions for next unshare
+    // unsharing user first because it sets proper permissions for next unshare
     unshare_ns("user", cont_pid_str); 
     unshare_ns("pid", cont_pid_str);
 
-    if (pipe2(synch_pipe, O_CLOEXEC) != 0) {
-        aucont::stdlib_error("Unable to open pipe");
-    }
-
-    auto slave_pid = fork();
-    if (slave_pid < 0) {
+    auto cmd_pid = fork();
+    if (cmd_pid < 0) {
         aucont::stdlib_error("Fork failed");
-    } else if (slave_pid > 0) {
-        // TODO: close pipes
-        aucont::write_to_pipe(get_slave_pid_pipe[1], slave_pid);
-        if (waitpid(slave_pid, NULL, 0) < 0) {
+    } else if (cmd_pid > 0) {
+        close(synch_pipe[0]);
+
+        // setting up cgroup if needed
+        if (cont.cpu_perc != 100) {
+            auto cg_tasks_file = aucont::get_cgrouph_path() + "/" + aucont::get_cgroup_for_cpuperc(cont.cpu_perc) + "/tasks";
+            std::ofstream out(cg_tasks_file, std::ios_base::out | std::ios_base::app);
+            out << cmd_pid;
+            out.close();
+        }
+        aucont::write_to_pipe(synch_pipe[1], true);
+        close(synch_pipe[1]);
+        
+        if (waitpid(cmd_pid, NULL, 0) < 0) {
             aucont::stdlib_error("Waitpid failed");
         }
         exit(0);
     }
-    // TODO: close pipes
+    close(synch_pipe[1]);
 
-    // applying other namespaces
-    std::array<std::string, 4> nss = {"net", "ipc", "uts", "mnt"}; // order is crucial
-    for (auto ns : nss) {
-        unshare_ns(ns, cont_pid_str);
-    }
+    unshare_ns("net", cont_pid_str);
+    unshare_ns("ipc", cont_pid_str);
+    unshare_ns("uts", cont_pid_str);
+    unshare_ns("mnt", cont_pid_str);
 
-    // synch...
+    // waiting for cgroup to be configured 
     aucont::read_from_pipe<bool>(synch_pipe[0]);
+    close(synch_pipe[0]);
 
+    const int max_arg_num = 100;
+    char* cmd = argv[2];
+    char* args[max_arg_num];
+    for (int j = 2; j < argc; ++j) {
+        args[j - 2] = argv[j];
+    }
+    args[argc - 2] = NULL;
     if (execvp(cmd, args) < 0) {
         aucont::stdlib_error("exec failed");
     }

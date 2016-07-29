1d7ee999edefa57516ad8e1a5f97a80c8555e571
Egor Gorbunov <egor-mailbox@ya.ru>
2016-07-29 15:24:27 +0300

    Add loop back device config

--- src/aucont_start/scripts/setup_net_cont.bash
+++ src/aucont_start/scripts/setup_net_cont.bash
@@ -10,7 +10,7 @@ HOST_IP=$3
 
 ip addr add "${CONT_IP}/24" dev "${CONT_VETH}" && \
 ip link set "${CONT_VETH}" up && \
-ip route add default via "${HOST_IP}"
-# ip link set lo up
+ip route add default via "${HOST_IP}" && \
+ip link set lo up
 
 exit $?
